ccda85ec3445555b3eaa2cea2a5ccc302a278952
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-11-27 19:25:18 +0530

    image fixes

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -1,5 +1,5 @@
 import { ImageData, useFetchData } from '@/hooks/fetchData'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import LoadingView from './LoadingView'
 import ErrorView from './ErrorView'
 import DataView from './DataView'
@@ -44,6 +44,8 @@ const Panel = (props: Props) => {
         abortFetch
     } = useFetchData()
 
+    const [fitImage, setFitImage] = useState(false)
+
     const generated_image_src = data ? URL.createObjectURL(data.image_blob) : ""
 
     // requestState = 3
@@ -60,13 +62,18 @@ const Panel = (props: Props) => {
                 bg-url(${generated_image_src ? generated_image_src : ""})
             `}
         >
-            <img src={generated_image_src} className='absolute' alt="" />
+            <img 
+                src={generated_image_src} 
+                className={`absolute w-full h-full ${fitImage ? 'object-contain' : 'object-cover'}`} 
+                alt={data?.description}
+            />
             <div
                 className={`
                     overlay
                     absolute top-0 left-0
                     w-full h-full
-                    ${data ? 'bg-black/70' : 'bg-[#222]'}
+                    ${data ? 'bg-black/30' : 'bg-[#222]'}
+                    backdrop-blur-xl
                     flex flex-col
                     justify-center gap-6
                     items-center
@@ -87,6 +94,16 @@ const Panel = (props: Props) => {
                     isLoading
                         ? <LoadingView cancelFunction={abortFetch} />
                         : <Input buttonOnClick={fetchData} requestState={requestState} />
+                }
+                {
+                    requestState === 1 && (
+                        <button 
+                            className='absolute bottom-2 right-2' 
+                            onClick={() => setFitImage(!fitImage)}
+                        >
+                            {fitImage ? 'Fill' : 'Fit'} Image
+                        </button>
+                    )
                 }
                 {/* {
                     requestState == 0
