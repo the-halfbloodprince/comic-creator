9cc882585db32142e6491589669378132ca28535
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-11-27 16:21:49 +0530

    template 1 grid

--- components/ComicStripTemplate1.tsx
+++ components/ComicStripTemplate1.tsx
@@ -1,14 +1,37 @@
 import React, { FC } from 'react'
+import Panel from './Panel'
+import './ComicStripTemplate1.css'
 
 type Props = {}
 
 const ComicStripTemplate1: FC<Props> = () => {
+
+  const numOfPanels = 10
+
   return (
-    <div>
+    <div className='w-full px-auto'>
         <h3
-          className='text-center text-2xl my-3'
-          my-2
+          className='text-center text-2xl my-3 w-fit mx-auto px-2'
+          contentEditable
         >Template 1</h3>
+        <div
+            className='
+                w-full max-w-6xl mx-auto
+                p-2
+
+                bg-white
+
+                grid gap-3
+                grid-cols-1
+                md:grid-cols-2
+                lg:grid-cols-3
+            '
+        >
+            {
+                Array.from({length: numOfPanels}, (_, i) => i+1)
+                    .map(i => <Panel key={i} />)
+            }
+        </div>
     </div>
   )
 }
---
+++ components/Panel.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+const Panel = (props: Props) => {
+  return (
+    <div
+        className='
+            panel 
+            h-72 lg:h-96
+            border-4 border-black
+            bg-white text-red-600
+        '
+    >Panel</div>
+  )
+}
+
+export default Panel
